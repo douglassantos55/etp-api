cd7a5b4d1061201a0918ceffdadda4e7177900d8
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-10-16 20:33:47 -0300

    refactor: websocket

--- main.go
+++ main.go
@@ -5,26 +5,10 @@ import (
 	"api/resource"
 	"api/server"
 	"api/warehouse"
-	"fmt"
 	"log"
-	"net/http"
 
 	"github.com/gorilla/websocket"
 )
-
-type Message struct {
-	Token   string
-	Message string
-}
-
-type Connection struct {
-	Token  string
-	Socket *websocket.Conn
-}
-
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
 
 func main() {
 	conn, err := database.GetConnection(database.SQLITE, "development.db")
@@ -32,59 +16,24 @@ func main() {
 		log.Fatalf("could not connect to database: %s", err)
 	}
 
-	events := make(chan Message)
-	connections := make(chan Connection)
+	events := make(chan server.Message)
+	connections := make(chan server.Connection)
 	disconnections := make(chan string)
+
 	svr := server.NewServer()
 	resource.CreateEndpoints(svr, conn)
 	warehouse.CreateEndpoints(svr, conn)
 
-	e.GET("/", func(c echo.Context) error {
-		events <- Message{Message: "Hello, World"}
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-
-	e.GET("/private", func(c echo.Context) error {
-		token := c.QueryParam("token")
-
-		events <- Message{
-			Token:   token,
-			Message: fmt.Sprintf("private message for %s", token),
-		}
-
-		return c.String(http.StatusOK, fmt.Sprintf("Private page for %s", token))
-	})
-
-	e.GET("/ws", func(c echo.Context) error {
-		ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-		if err != nil {
-			log.Print("upgrade: ", err)
-			return err
-		}
-
-		defer ws.Close()
-		var userToken string
-
-		for {
-			_, token, err := ws.ReadMessage()
-			if err != nil {
-				log.Print("read: ", err)
-				disconnections <- string(userToken)
-				break
-			}
-			userToken = string(token)
-			connections <- Connection{Token: userToken, Socket: ws}
-		}
-
-		return nil
-	})
+	svr.GET("/", server.Greeting(events))
+	svr.GET("/private", server.Private(events))
+	svr.GET("/ws", server.WS(connections, disconnections))
 
 	go processEvents(events, connections, disconnections)
 
 	svr.Start(":1323")
 }
 
-func processEvents(events <-chan Message, connections <-chan Connection, disconnections <-chan string) {
+func processEvents(events <-chan server.Message, connections <-chan server.Connection, disconnections <-chan string) {
 	sockets := make(map[string]*websocket.Conn)
 
 	for {
--- server/server.go
+++ server/server.go
@@ -1,14 +1,32 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gookit/validate"
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type Validator struct{}
+type (
+	Validator struct{}
+
+	Message struct {
+		Token   string
+		Message string
+	}
+
+	Connection struct {
+		Token  string
+		Socket *websocket.Conn
+	}
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
 
 func (v *Validator) Validate(i any) error {
 	validation := validate.Struct(i)
@@ -27,4 +45,45 @@ func NewServer() *echo.Echo {
 	e.Validator = &Validator{}
 
 	return e
+}
+
+func Greeting(events chan<- Message) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		events <- Message{Message: "Hello, World"}
+		return c.String(http.StatusOK, "Hello, World!")
+	}
+}
+
+func Private(events chan<- Message) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		token := c.QueryParam("token")
+		events <- Message{
+			Token:   token,
+			Message: fmt.Sprintf("private message for %s", token),
+		}
+		return c.String(http.StatusOK, fmt.Sprintf("Private page for %s", token))
+	}
+}
+
+func WS(connections chan<- Connection, disconnections chan<- string) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+		if err != nil {
+			return err
+		}
+
+		defer ws.Close()
+		var userToken string
+
+		for {
+			_, token, err := ws.ReadMessage()
+			if err != nil {
+				disconnections <- string(userToken)
+				break
+			}
+			userToken = string(token)
+			connections <- Connection{Token: userToken, Socket: ws}
+		}
+		return nil
+	}
 }
